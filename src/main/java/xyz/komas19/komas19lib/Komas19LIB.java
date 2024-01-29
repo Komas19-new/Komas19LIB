f67affc5467ce35c640216fa70750e54e0485ff6
Komas19-new <benoitbourcier@outlook.com>
2024-01-29 16:04:13 +0100

    import

---
+++ src/main/java/xyz/komas19/komas19lib/Komas19LIB.java
@@ -0,0 +1,17 @@
+package xyz.komas19.komas19lib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Komas19LIB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
